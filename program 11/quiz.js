6ce3fe219616525fcabb1f9561ab50d07f41f324
Murshid TP <murshidtp68@gmail.com>
2021-03-29 20:27:14 +0530

    some files modified

--- program 11/quiz.js
+++ program 11/quiz.js
@@ -15,21 +15,21 @@ const scoreDiv = document.getElementById("scoreContainer");
 let questions = [
     {
         question : "What does HTML stand for?",
-        imgSrc : "img/html.png",
+        imgSrc : "download.jpeg",
         choiceA : "Correct",
         choiceB : "Wrong",
         choiceC : "Wrong",
         correct : "A"
     },{
         question : "What does CSS stand for?",
-        imgSrc : "img/css.png",
+        imgSrc : "CSS.png",
         choiceA : "Wrong",
         choiceB : "Correct",
         choiceC : "Wrong",
         correct : "B"
     },{
         question : "What does JS stand for?",
-        imgSrc : "img/js.png",
+        imgSrc : "js.png",
         choiceA : "Wrong",
         choiceB : "Wrong",
         choiceC : "Correct",
